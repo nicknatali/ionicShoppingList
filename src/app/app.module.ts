58470a3d6888c79f650b435f85ee51f553eb53d1
NickNatali <nicka.natali@calbaptist.edu>
2018-01-30 20:59:08 -0800

    Added a new page to the shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,25 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2';
+import { FIREBASE_CONFIG } from './firebase.credentials';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBinLGjfoGgxnqoIs4K1a9TNme5C43LnvE",
+    authDomain: "ionicshoppinglistforpros.firebaseapp.com",
+    databaseURL: "https://ionicshoppinglistforpros.firebaseio.com",
+    projectId: "ionicshoppinglistforpros",
+    storageBucket: "",
+    messagingSenderId: "413011707222"
+  };
